cdb2c5f6ce0155b406e384dce40f8e16174fd8fd
m.garmabi <m.garmabi@khodro45.com>
2025-11-17 11:43:29 +0330

    Initial commit with project setup and basic structure established.

---
+++ practice/1.py
@@ -0,0 +1,15 @@
+x = 'Hi this is a string'
+
+# pep8 style
+
+# lower the string
+x = x.lower()
+print(x)
+
+# split the string into a list
+x = x.split()
+print(x)
+
+# join list into a string with space
+x = " ".join(x)
+print(x)
