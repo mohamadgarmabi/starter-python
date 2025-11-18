9c8ba89279443d5309177f5a266f0496789b7388
m.garmabi <m.garmabi@khodro45.com>
2025-11-18 14:20:19 +0330

    update some code

---
+++ quiz/quiz-04.py
@@ -0,0 +1,6 @@
+numbers = [1, 2, 3, 4, 5]
+print(numbers[2])
+
+# add to last of list
+numbers.append(6)
+print(numbers)
---
+++ quiz/quiz-05.py
@@ -0,0 +1,9 @@
+person = {"name": "Ali", "age": 25}
+print(person["name"])
+
+
+# add new item to person
+person["city"] = "Tehran"
+
+# get all keys from person
+print(person.keys())
